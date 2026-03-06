bb365720c85e5f3bbaae97620681fd4205f8bf8b
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-03-06 15:57:41 +0700

    Change tag a to Link

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 const Header = () => {
     return (
         <ul>
-            <li><a className="active" href="#home">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Product</a></li>
+            <li><Link className="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/products">Products</Link></li>
         </ul>
     )
 }
